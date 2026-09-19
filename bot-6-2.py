6802c38fbffcae1ec03eddf30b09da0db638d15b
Dados081g <egorevseev062@gmail.com>
2026-09-19 23:35:42 +0300

    Update bot-6-2.py

--- bot-6-2.py
+++ bot-6-2.py
@@ -26,7 +26,7 @@ from aiogram.types import (
 )
 
 # ══════════════════════════ НАСТРОЙКИ ══════════════════════════
-BOT_TOKEN = os.getenv("BOT_TOKEN", "8763383205:AAEBAEq_AHc7AR2ReuEnLtzaDtfUIr2roPI)
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8763383205:AAEBAEq_AHc7AR2ReuEnLtzaDtfUIr2roPI")
 
 # Вход в админку: команда  /admin ПАРОЛЬ
 # После первого входа бот запоминает вас, дальше хватает просто /admin.
